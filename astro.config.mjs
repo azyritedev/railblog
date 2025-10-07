8937b942753583bfd27fda0393939b27f4de1125
azyrite <azyrite@astrial.org>
2025-10-07 19:05:42 +1100

    feat: add umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
 import content from "@originjs/vite-plugin-content";
@@ -7,6 +8,11 @@ import content from "@originjs/vite-plugin-content";
 import node from "@astrojs/node";
 
 import sitemap from "@astrojs/sitemap";
+
+import umami from "@yeskunall/astro-umami";
+
+// @ts-expect-error NODE_ENV cannot be forced into not-null using `!` as thi is an .mjs file
+const { PUBLIC_UMAMI_ID, PUBLIC_UMAMI_ENDPOINT_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -33,5 +39,13 @@ export default defineConfig({
     mode: "standalone",
   }),
 
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    umami({
+      // @ts-expect-error See above expect-error declaration
+      id: PUBLIC_UMAMI_ID,
+
+      endpointUrl: PUBLIC_UMAMI_ENDPOINT_URL
+    }),
+  ],
 });
